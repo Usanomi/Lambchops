3f75cb6141dc0a347fb2eb155b640ab694ea085e
Fady Youssef <Thefady4@gmail.com>
2024-03-23 07:13:50 +0000

    push

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
+/*
+Team Members: Steph Borla and Fady Youssef
+Course: CPSC 122
+Date Submitted: 3/20/2024
+Assignment Name: Project 7
+Description: 
+*/
 #include <iostream>
+using namespace std;
+
+class Test {
+private:
+int length;
+int width;
+public:
+int add()
+{
+  return length + width;
+}
+};
+
+int Test::push()
+{
+  
+}
 
 int main() {
   std::cout << "Hello World!\n";
